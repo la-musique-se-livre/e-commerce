5bc9257c7b87485c63100349b5191eea85322e57
WoZe-Clan <woze94@outlook.fr>
2021-04-08 16:29:15 +0200

    changement de la bare

--- index.php
+++ index.php
@@ -29,26 +29,71 @@
           />
           <button id="loupe" type="submit" class="icone-loupe"></button>
 
-          <div class="m-right">
-            <nav class="l-menu" role="navigation">
-              <a href="http:www.exemple-de-livres.com"
-                ><i class="fa fa-bars" aria-hidden="true"></i> Livres
-              </a>
-              <a href=" http:www.exemple-de-cd.com  "
-                ><i class="fa fa-bars" aria-hidden="true"></i> CDs
-              </a>
-              <a href=" http:www.exemple-de-vinyless.com  "
-                ><i class="fa fa-bars" aria-hidden="true"></i> Vinyles
-              </a>
-              <a href=" http:www.exemple-de-contact.com  "> Nous contacter </a>
-              <nav class="connecter">
-                <a href=" login/index.php "> Se connecter </a>
-              </nav>
-              <a href=" http:www.exemple-de-panier.com  "
-                ><i class="fa fa-shopping-basket" aria-hidden="true"></i
-              ></a>
-            </nav>
-          </div>
+          <nav>
+            <ul class="menu1">
+                <li><a href="#" title="">Home</a></li>
+                <li class="services"><a href="#" title="">Services</a>
+                    <ul class="sous-menu">
+                    <li><a href="#" title="">Nouveautés</a></li>
+                    <li><a href="#" title="">Coups de coeur</a></li>
+                    <li><a href="#" title="">Polar/Thriller</a></li>
+                    <li><a href="#" title="">Romance</a></li>
+                    <li><a href="#" title="">Littérature Etrangère</a></li>
+                    <li><a href="#" title="">Aventure/Ado</a></li>
+                    <li><a href="#" title="">Cuisine/Patisserie</a></li>
+                    <li><a href="#" title="">Livres Scolaire</a></li>
+                    <li><a href="#" title="">Livres Universitaires</a></li>
+                    <li><a href="#" title="">Dictionnaires</a></li>
+                    <li><a href="#" title="">Les Touts Petits</a></li>
+                    <li><a href="#" title="">Poésie</a></li>
+                    <li><a href="#" title="">Classiques</a></li>
+                    </ul>
+
+                </li>
+                <li class="services"><a href="#" title="">Livres</a>
+                    <ul class="sous-menu">
+                        <li><a href="#" title="">Service 1</a></li>
+                        <li><a href="#" title="">Service 2</a></li>
+                        <li><a href="#" title="">Service 3</a></li>
+                        <li><a href="#" title="">Service 3</a></li>
+                        <li><a href="#" title="">Service 3</a></li>
+                        <li><a href="#" title="">Service 3</a></li>
+                        
+                    </ul>
+
+                </li>
+                <li class="services"><a href="#" title="">CDs</a>
+                    <ul class="sous-menu">
+                        <li><a href="#" title="">Service 1</a></li>
+                        <li><a href="#" title="">Service 2</a></li>
+                        <li><a href="#" title="">Service 3</a></li>
+                        <li><a href="#" title="">Service 3</a></li>
+                        <li><a href="#" title="">Service 3</a></li>
+                        <li><a href="#" title="">Service 3</a></li>
+                        <li><a href="#" title="">Service 3</a></li>
+                        <li><a href="#" title="">Service 3</a></li>
+                    </ul>
+
+                </li>
+
+                <li class="services"><a href="#" title="">Vinyles</a>
+                    <ul class="sous-menu">
+                        <li><a href="#" title="">Service 1</a></li>
+                        <li><a href="#" title="">Service 2</a></li>
+                        <li><a href="#" title="">Service 3</a></li>
+                        <li><a href="#" title="">Service 3</a></li>
+                        <li><a href="#" title="">Service 3</a></li>
+                        <li><a href="#" title="">Service 3</a></li>
+                        <li><a href="#" title="">Service 3</a></li>
+                        <li><a href="#" title="">Service 3</a></li>
+                    </ul>
+
+                </li>
+
+                <li> <a href =" http:www.exemple-de-panier.com  "><i class="fa fa-shopping-basket" aria-hidden="true"></i></a></li>
+                
+            </ul>
+        </nav>
         </div>
       </nav>
     </header>
